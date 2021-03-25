e180b4297f8e594b6449da3533eecf8f9d31f03d
Hayden Jones <haychjones@gmail.com>
2021-03-25 18:28:18 +1100

    styling

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var today = moment().format('MMMM Do YYYY, h:mm:ss a');
 var currentTime = document.getElementById("currentTime")
+var note = document.getElementById("note")
+
 
 function renderClock() {
     currentTime.innerHTML = today
 }
 
-
-
 setInterval(renderClock, 1000);
+
+
+
 
 
 
