a8d3609efa5a3ab333628b193075dea6434ac63f
Hayden Jones <haychjones@gmail.com>
2021-03-30 15:08:45 +1100

    Added local storage function

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-var today = moment().format('MMMM Do YYYY, h:mm:ss a');
-var currentTime = document.getElementById("currentTime")
-var note = document.getElementById("note")
+var today = moment().format('MMMM Do YYYY');
+var currentTime = document.getElementById("currentTime");
+var note = document.getElementById("note");
 
-
-function renderClock() {
-    currentTime.innerHTML = today
+function revertToToday() {
+    currentTime.innerHTML = today;
 }
 
-setInterval(renderClock, 1000);
+revertToToday();
 
+function changeDate() {
+    prompt("Enter Date", "DD/MM/YYYY")
+}
 
+function storeNote(time) {
+    var userInput = document.getElementById(time).value;
+    if (localStorage.getItem(time) !== ''){
+        localStorage.removeItem(time);
+    }
+    window.localStorage.setItem(time, userInput);
+}
 
-
-
+function getNote(time) {
+    var storedNote = localStorage.getItem(time);
+    if (storedNote !== ''){
+        document.getElementById(time).value = storedNote;
+        console.log(storedNote)
+    }
+}
+for (var i = 9; i < 12; i++) getNote(i + "am");
+getNote("12pm")
+for (var i = 1; i < 6; i++) getNote(i + "pm");
 
