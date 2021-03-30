24aa55aff59e083b5343e1051d17e07aec9e406c
Hayden Jones <haychjones@gmail.com>
2021-03-30 23:47:01 +1100

    end product

--- script.js
+++ script.js
@@ -4,30 +4,39 @@ var note = document.getElementById("note");
 
 function revertToToday() {
     currentTime.innerHTML = today;
+    getNoteLoop();
 }
 
 revertToToday();
 
 function changeDate() {
-    prompt("Enter Date", "DD/MM/YYYY")
+    var userDate = prompt("Enter Date MM/DD/YYYY", "MM/DD/YYYY");
+    userDate = moment(userDate).format('MMMM Do YYYY')
+    currentTime.innerHTML = userDate;
+    getNoteLoop();    
 }
 
 function storeNote(time) {
     var userInput = document.getElementById(time).value;
+    var key = String(time + " " + currentTime.innerHTML)
     if (localStorage.getItem(time) !== ''){
         localStorage.removeItem(time);
     }
-    window.localStorage.setItem(time, userInput);
+    window.localStorage.setItem(key, userInput);
 }
 
 function getNote(time) {
-    var storedNote = localStorage.getItem(time);
+    var key = String(time + " " + currentTime.innerHTML)
+    var storedNote = localStorage.getItem(key);
+    console.log(storedNote);
     if (storedNote !== ''){
         document.getElementById(time).value = storedNote;
-        console.log(storedNote)
     }
-}
-for (var i = 9; i < 12; i++) getNote(i + "am");
-getNote("12pm")
-for (var i = 1; i < 6; i++) getNote(i + "pm");
 
+}
+
+function getNoteLoop(){
+    for (var i = 9; i < 12; i++) getNote(i + "am");
+    getNote("12pm")
+    for (var i = 1; i < 6; i++) getNote(i + "pm");
+}
